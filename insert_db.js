13ea2034934a39c8b83d9e7845b6d347d4b8eaf6
Prasenjit Bhattacharjee <prasn94@gmail.com>
2022-04-08 18:04:29 +0530

    Update insert_db.js

--- insert_db.js
+++ insert_db.js
@@ -10,8 +10,8 @@ import utf8 from 'utf8';
 functions:
         Function Name                   Description
     fetch_Data            :     function to fetch data from LastRaw API
-    convert_data          :     function to convert JSON from LastRaw API to Documents
-    write_data            :     function to insert Documents into MongoDB
+    convert_data          :     function to transform JSON recieved from LastRaw API
+    write_data            :     function to insert lines of data into Graphite
     get_token             :     function to get Access token 
 */
 
@@ -22,15 +22,16 @@ const client_secret = config.client_secret;
 const test_types = config.tests;
 const server = config_graphite.carbon_server;
 const port = config_graphite.carbon_port;
+const prefix = "catchpoint.testdata";
 
 // main function to fetch and store data
 async function run() {
     try {
-        let token = await get_token(client_key, client_secret);
-        var tests_list = [];
+        const token = await get_token(client_key, client_secret);
+        let tests_list = [];
         // breakdown the tests list into chunks of 20 test ids for each test type
         Object.keys(test_types).forEach(function (key, index) {
-            var temp = [], chunk = 20;
+            let temp = [], chunk = 20;
             for (let i = 0, j = test_types[key].length; i < j; i += chunk) {
                 temp.push(test_types[key].slice(i, i + chunk));
             }
@@ -38,7 +39,7 @@ async function run() {
         });
         for (let tests of tests_list) {
             for (let arr of tests) {
-                var url = `${raw_data_url}${arr}`;
+                let url = `${raw_data_url}${arr}`;
                 let raw_data = await fetch_Data(token, url);
                 let data = convert_data(raw_data);
                 if (data != "No Data") {
@@ -55,79 +56,74 @@ async function run() {
         log.error(error);
     }
 }
-
 // function to fetch Raw Data
-function fetch_Data(token, url) {
-    return new Promise((resolve, reject) => {
-        fetch(url, {
-            headers: {
-                'accept': 'application/json',
-                'authorization': `Bearer ${token}`
-            }
-        })
-            .then(res => res.json())
-            .then(json => {
-                // if object has property Message ,display Error, else Process Data
-                if (json.hasOwnProperty('Message')) {
-                    log.error(`${json.Message}`);
-                    reject(json.Message)
-                } else {
-                    log.info(`<<Fetched Raw Test Data>> ${url} Raw Data Start Timestamp: ${json.start} End Timestamp: ${json.end}`)
-                    if (json.hasOwnProperty('error')) {
-                        log.error(`${json.error}`, "<<Check Catchpoint configuration file>>")
-                    }
-                    resolve(json)
+async function fetch_Data(token, url) {
+    let response = await fetch(url, {
+        headers: {
+            'accept': 'application/json',
+            'authorization': `Bearer ${token}`
+        }
+    })
+        .then(res => res.json())
+        .then(json => {
+            // if object has property Message, display Error, else Process Data
+            if (json.hasOwnProperty('Message')) {
+                log.error(`${json.Message}`);
+            } else {
+                log.info("<<Fetched Raw Test Data>>", url, `Raw Data Start Timestamp: ${json.start} End Timestamp: ${json.end}`)
+                if (json.hasOwnProperty('error')) {
+                    log.error(`${json.error}`, "<<Check Catchpoint configuration file>>")
                 }
-
-            }).catch(err => {
-                log.error(err)
-                reject(err)
+                return json;
             }
-            );
-    });
+        }).catch(err => {
+            log.error(err);
+        }
+        );
+    return response;
 }
-// function to parse and convert JSON received from api to document structure for mongodb
+// function to parse and transform JSON 
 function convert_data(structure) {
     // Checks if there is test data for the last 15 mins
     if (structure['detail'] != null) {
 
-        var items = []
-        var test_params = []
-        var test_metric_values = []
-        var temp = {}
-        var solution = {}
-
-        for (let value of structure['detail']['fields']['synthetic_metrics']) {
-            var metrics = value['name'].replace(/ /g, '') //Remove whitespace from metric names
+        let items = []
+        let test_params = []
+        let test_metric_values = []
+        let temp = {}
+        let solution = {}
+        for (let value of structure?.detail?.fields?.synthetic_metrics) {
+            let metrics = value['name'].replace(/ /g, '') //Remove whitespace from metric names
             test_params.push(metrics)
         }
-
-        for (let value of structure['detail']['items']) {
-            var metric_values = value['synthetic_metrics']
-            var flag = true
-            var temp = {}
+        for (let value of structure?.detail?.items) {
+            let metric_values = value['synthetic_metrics']
+            let flag = true
+            let temp = {}
             temp.tags = {}
             temp.timestamp = {}
             for (let i in value) {
                 if (i != 'synthetic_metrics') {
-                    if (i == 'dimension') {
-                        temp.timestamp = Math.round(new Date(value[i]['name']).getTime()/1000); //Epoch time in seconds precision
-                    }
-                    if (i == 'breakdown_1') {
-                        temp.tags['testId'] = value[i]['id']
-                    }
-                    if (i == 'breakdown_2') {
-                        temp.tags['nodeId'] = value[i]['id']
-                    }
-                    if (i == 'hop_number') {
-                        temp.tags[i] = value[i]
-                    }
-                    if (i == 'step') {
-                        temp.tags[i] = value[i]
+                    switch (i) {
+                        case "dimension":
+                            temp.timestamp = Math.round(new Date(value[i]['name']).getTime()/1000); //Epoch time in seconds precision
+                            break;
+                        case "breakdown_1":
+                            temp.tags['testId'] = value[i]['id']
+                            break;
+                        case "breakdown_2":
+                            temp.tags['nodeId'] = value[i]['id']
+                            break;
+                        case "hop_number":
+                            temp.tags[i] = value[i]
+                            break;
+                        case "step":
+                            temp.tags[i] = value[i]
+                            break;
                     }
                 }
             }
-            if (flag == true) {
+            if (flag) {
                 metric_values.push(temp)
                 test_metric_values.push(metric_values)
             }
@@ -145,7 +141,7 @@ function convert_data(structure) {
             }
             items.push(temp)
         }
-        solution['items'] = items;
+        solution['items'] = items
         return solution['items'];
     }
     else {
@@ -153,12 +149,11 @@ function convert_data(structure) {
         return ("No Data");
     }
 }
-
-//function to send data to graphite carbon
+// function to send lines of data to Graphite carbon
 async function write_data(items) {
     /*
-      Convert json to lines of metrics in the format- my.series;tag1=value1;tag2=value2 metric_value timestamp
-      Then send the lines of metrics to carbon listener for storage. Metric path: system.cp.testdata.MetricName
+      Transform JSON items to lines of metrics in the format- my.series;tag1=value1;tag2=value2 metric_value timestamp
+      Then send the lines of metrics to Graphite's carbon listener for storage. Metric path: catchpoint.testdata.MetricName
     */
     try {
         log.info("<<#Items>>", items.length)
@@ -167,17 +162,16 @@ async function write_data(items) {
             for (let key in item['metrics'])
             {
                 if ('step' in item['tags']) {
-                    lines.push(`system.cp.testdata.${key};testId=${item['tags']['testId']};nodeId=${item['tags']['nodeId']};stepNumber=${item['tags']['step']} ${item['metrics'][key]} ${item['timestamp']}`)
+                    lines.push(`${prefix}.${key};testId=${item['tags']['testId']};nodeId=${item['tags']['nodeId']};stepNumber=${item['tags']['step']} ${item['metrics'][key]} ${item['timestamp']}`)
                 }
                 else if ('hop_number' in item['tags']) {
-                    lines.push(`system.cp.testdata.${key};testId=${item['tags']['testId']};nodeId=${item['tags']['nodeId']};hopNumber=${item['tags']['hop_number']} ${item['metrics'][key]} ${item['timestamp']}`)
+                    lines.push(`${prefix}.${key};testId=${item['tags']['testId']};nodeId=${item['tags']['nodeId']};hopNumber=${item['tags']['hop_number']} ${item['metrics'][key]} ${item['timestamp']}`)
                 }
                 else {
-                    lines.push(`system.cp.testdata.${key};testId=${item['tags']['testId']};nodeId=${item['tags']['nodeId']} ${item['metrics'][key]} ${item['timestamp']}`)
+                    lines.push(`${prefix}.${key};testId=${item['tags']['testId']};nodeId=${item['tags']['nodeId']} ${item['metrics'][key]} ${item['timestamp']}`)
                 }
             }
         }
-
         let message = lines.join('\n') //all lines must end in a newline
         log.info("<<#Series to update>>",message.length)
         // open a connection and send lines of data as socket message to carbon listener over TCP
@@ -195,4 +189,3 @@ async function write_data(items) {
 }
 
 run();
-
